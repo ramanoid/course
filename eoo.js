983c18677569d37763113ab999e85847bc5c648b
ramanoid <ramanunnim1995@gmail.com>
2019-03-26 15:50:32 +0530

    fgfg

---
+++ eoo.js
@@ -0,0 +1,5 @@
+var x=2;
+if(x%2 == 0)
+console.log("yeah");
+else
+console.log("no");
--- greater.js
+++ greater.js
@@ -1,11 +1,11 @@
-var x=2;
+var x=20;
 var y=5;
 var z=10;
 if(x>y)
 {if(x>z)
 console.log(x);}
 if (y>z)
-{if (y>z)
+{if (y>x)
 console.log(y);}
 else
 console.log(z);
