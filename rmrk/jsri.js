62adef6d43e0b637a1a55a58c8925e0be860aad0
ramanoid <ramanunnim1995@gmail.com>
2019-03-27 15:51:22 +0530

    mmjh

---
+++ rmrk/jsri.js
@@ -0,0 +1,12 @@
+function read()
+{
+    console.log("reading doone");
+    var n=document.getElementById("name").value;
+    var x=document.getElementById("email").value;
+    var y=document.getElementById("clg").value;
+    var z=document.getElementById("admn_no").value;
+    console.log(n);
+    console.log(x);
+    console.log(y);
+    console.log(z);
+}
