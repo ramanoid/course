6946a6e0a0fa193f65db28d806855b048c41e0cf
ramanoid <ramanunnim1995@gmail.com>
2019-04-08 14:12:50 +0530

    hg

---
+++ blockoid/nodejstut/add.js
@@ -0,0 +1,15 @@
+var a; var b;
+console.log('this is the add mod \n');
+module.exports.addn=(a,b)=>{ // this is es6 funstion def
+
+   return (a+b);
+}
+module.exports.sbn=(a,b)=>{         
+    return a-b;
+}
+module.exports.dvn=(a,b)=>{
+      return (a/b);
+}
+module.exports.mln=(a,b)=>{
+    return a*b;
+}
